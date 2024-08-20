ae98dd943191c16f43c9c328c45366ca885254e1
Nikhil Sarwara <117506262+Nikhil-Sarwara@users.noreply.github.com>
2024-08-20 13:40:53 +1000

    NOde Red and NOdejs Server Done :+1:

---
+++ src/data generation/nodejs-server/server.js
@@ -0,0 +1,42 @@
+// nodejs-server/server.js
+const express = require("express");
+const http = require("http");
+const mqtt = require("mqtt");
+
+const app = express();
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+// MQTT client setup
+const mqttClient = mqtt.connect("broker.hivemq.com", {
+  port: 8883,
+});
+
+mqttClient.on("connect", () => {
+  console.log("Connected to HiveMQ broker");
+  mqttClient.subscribe("data_topic", (err) => {
+    if (err) {
+      console.error("Subscription error:", err);
+    }
+  });
+});
+
+mqttClient.on("message", (topic, message) => {
+  // Handle incoming MQTT messages
+  const data = JSON.parse(message.toString());
+  console.log("Received data:", data);
+
+  // Broadcast data to all clients using WebSocket (optional)
+  io.emit("data", data);
+});
+
+// Basic route
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// Start server
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
